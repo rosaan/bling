6d297af5b0c253ac7ce4a667acc87b956acb2807
Rosaan Ramasamy <rosaan@antdragon.co>
2024-01-25 13:44:54 +0800

    upgrade stripity_stripe to v3

--- mix.exs
+++ mix.exs
@@ -44,7 +44,7 @@ defmodule Bling.MixProject do
       {:phoenix, "~> 1.7.2"},
       {:phoenix_live_view, "~> 0.20"},
       {:plug, "~> 1.14"},
-      {:stripity_stripe, "~> 2.17"},
+      {:stripity_stripe, "~> 3.1"},
       {:postgrex, ">= 0.0.0", only: :test},
       {:faker, "~> 0.17", only: :test},
       {:jason, "~> 1.4"}
